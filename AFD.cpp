cc8624bfbfbf53653c9382c31bad1347610c56c1
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-04-11 20:54:41 -1100

    Update AFD.cpp

--- AFD.cpp
+++ AFD.cpp
@@ -24,11 +24,13 @@ public:
             if (find(alphabet.begin(), alphabet.end(), c) == alphabet.end()) 
             {
                 // Daca starea incearca sa tranzitioneze la o stare care nu exisita in alfabet
+                // If the current state tries to transitionate to a state that doesn't exists
                 return {};
             }
             if (transition_table[current_state].count(c) == 0) 
             {
                 // Daca nu exista tranzitie pentru starea curenata
+                // If there are no transitions from the current state
                 return {};
             }
             current_state = transition_table[current_state][c];
@@ -37,9 +39,11 @@ public:
         if (find(accept_states.begin(), accept_states.end(), current_state) != accept_states.end()) 
         {
             // Daca starea finala este o stare acceptata se returneaza un vector cu stari
+            // if the final state is and accepted state a vector that contains the states is returned
             return states;
         } else {
             //In caz contrar se va afisa un veactor gol
+            // Else an empty vector will be returned
             return {};
         }
     }
@@ -47,9 +51,10 @@ public:
 
 int main(){
 
-    int num_states = 4; // numarul starilor
-    vector<char> alphabet = {'0', '1', '2'}; //alfabetul
+    int num_states = 4; // numarul starilor // the number of states
+    vector<char> alphabet = {'0', '1', '2'}; //alfabetul /// the alphabet
     //Aici se introduc tranzitiile. Tabelul de tranzitii de mai jos e cel de la exemplul 2:
+    // Here you have to add the tranzitions
     vector<map<char,int>> transition_table = {{ {'0', 1}, {'1', 0} },
                                               { {'0', 2}, {'1', 0}},
                                               { {'2', 3} },
@@ -60,10 +65,10 @@ int main(){
     string input = "110101002";
     vector<int> states = my_afd.run(input);
     if (states.empty()) {
-        cout << "Cuvantul nu e valid" << endl;
+        cout << "Cuvantul nu e valid/The word is not valid" << endl;
     } else {
-        cout << "Cuvantul este valid" << endl;
-        cout << "Drumul este: ";
+        cout << "Cuvantul este valid/The word is valid" << endl;
+        cout << "Drumul este: /The path is:  ";
         for (int state : states) {
             cout << state << " ";
         }
