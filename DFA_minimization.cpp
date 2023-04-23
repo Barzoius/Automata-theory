7f489ca4fba458e39558b4e5a25fc4e5c05c8f73
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-04-23 03:57:20 -1100

    Update DFA_minimization.cpp

--- DFA_minimization.cpp
+++ DFA_minimization.cpp
@@ -1,35 +1,45 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-//O sa adaug comentarii//
+int states;
+int Groups;
+int symbs; //Simbolurile tranzitiilor
+
 
 struct State{
 
-    int IDstate;
-    int IDgroup;
-    char Status;
-    int* PTRState;
-    void set(int);
+    int IDstate; // id/nunarul starii
+    int IDgroup; // id/numarul grupei(partiriei)
+    char Status; // stare finala sau non-finala
+    int* PTRState; // pointeaza la un vector care care contine IDstate-ul starilor traversate
+    void set(int, std::vector<int>); // Seteaza datele starii
 };
 
-void State::set(int symbs)
+void State::set(int symb, std::vector<int> finalStates)
 {
-    PTRState = new int[symbs];
+    PTRState = new int[symb];
     std::cout<<"State: q";
     std::cin>>IDstate;
     std::cout<<std::endl;
-    std::cout<<"Final/NonFinal state(f/n): ";
-    std::cin>>Status;
-    std::cout<<std::endl;
+    for(int i = 0; i < states; i++)
+    {
+        if(std::find(finalStates.begin(), finalStates.end(), IDstate) != finalStates.end()){
+            Status = 'n';
+        }else {
+            Status = 'y';
+        }
+    }
 
     if(Status == 'n')
         IDgroup = 0;
-    else if(Status == 'f')
+    else if(Status == 'y')
         IDgroup = 1;
 
     std::cout<<"Input the transitions: ";
     std::cout<<std::endl;
 
-    for(int i = 0; i < symbs; i++){
+    for(int i = 0; i < symb; i++){
 
         std::cout<<"Input: "<< i <<" q";
         std::cin>>PTRState[i];
@@ -38,17 +48,13 @@ void State::set(int symbs)
     }
 }
 
-int states;
-int Groups;
-int symbs;
-
 int MINIMIZATION(State s[])
 {
-    int Current_GROUP[states];
+    int Current_GROUP[states]; //Aici stochez IDgroup-ul pentru starea la care am ajuns
 
-    for(int i = 0; i < states; i++){
-        for(int j = 0; j < symbs; j++){
-            for(int k = 0; k < states; k++)
+    for(int i = 0; i < states ; i++){ //parcurge starile
+        for(int j = 0; j < symbs; j++){ //parcurge tranzitiile
+            for(int k = 0; k < states; k++)//
             {
                 int aux = s[k].PTRState[j];
                 Current_GROUP[k] = s[aux].IDgroup;
@@ -57,7 +63,7 @@ int MINIMIZATION(State s[])
 
             for(int z = 0; z < states - 1; z++)
             {
-                int cat_group[states];
+                int cat_group[states];// Arrayul asta tine cont de starile care creaza partitii noi
                 for(int k = 0; k < states; k++)
                 { cat_group[k] = -1; }
 
@@ -66,7 +72,7 @@ int MINIMIZATION(State s[])
                     if(s[z].IDgroup == s[t].IDgroup && Current_GROUP[z] != Current_GROUP[t])
                     {
                         int f = -1;
-                        for(int l = 0; l < states; l++)
+                        for(int l = 0; l < states; l++)//aici verificam daca s[t] poate sa apartina unei partiti existente ***
                         {
                             if(Current_GROUP[t] == cat_group[l])
                             {
@@ -75,7 +81,7 @@ int MINIMIZATION(State s[])
                             }
                         }
 
-                        if(f == -1)
+                        if(f == -1) // *** caz contrat cream o partitie noua
                         {
                             s[t].IDgroup = Groups;
                             Groups++;
@@ -87,54 +93,19 @@ int MINIMIZATION(State s[])
 
             }
         }
-
-        int equiv = i;
-        for(int g = 0; g < states; g++)
-        {
-            if(s[g].IDgroup != Current_GROUP[i])
-            {
-                equiv = -1;
-                break;
-            }
-        }
-
-        if(equiv != -1) { return equiv; }
     }
 }
 
-void run(State s[])
-{
 
-    for(int i = 0; i < states - 1; i++){
-        if(s[i].IDstate != -1){
-            for(int j = i + 1; j < states; j++){
-                if(s[i].IDgroup == s[j].IDgroup){
-                    for(int k = 0; k < states; k++){
-                        for(int z = 0; z < states; z++){
-                            if(s[k].PTRState[z] == s[j].IDstate)
-                            { s[k].PTRState[z] = i; }
-                        }
-                    }
-
-                    s[j].IDstate = -1;
-
-                    if(s[j].Status == 'f')
-                    { s[i].Status = 'f';}
-                }
-            }
-        }
-
-    }
-}
 void PrintPartitions(State s[])
 {
     std::cout<<"PARTITIONS: "<<std::endl;
-    for(int i=0;i<Groups;i++)
+    for(int i = 0; i < Groups; i++)
     {
         std::cout<<"{ ";
-        for(int j=0;j<states;j++)
+        for(int j = 0; j < states; j++)
         {
-            if(s[j].IDgroup==i)
+            if(s[j].IDgroup == i)
                 std::cout<<"q"<<s[j].IDstate<<" , ";
         }
        std:: cout<<"}\n";
@@ -145,17 +116,25 @@ int main()
 {
     std::cout<<"Enter number of states: ";
     std::cin>>states;
-    std::cout<<"Enter transition symbols: ";
+    std::cout<<"Enter number of input symbols: ";
     std::cin>>symbs;
-    State s[states];
-    for(int i=0;i<states;i++)
-    {
-        s[i].set(symbs);
+    std::cout<<"Enter number of final states: ";
+    int n;
+    std::cin>>n;
+    std::vector<int> finalStates;
+    for (int i = 0; i < n; i++) {
+        int finalState;
+        std::cout << "Enter the ID of final state: ";
+        std::cin >> finalState;
+        finalStates.push_back(finalState);
     }
-    Groups=2;  //Primele doua partitii
+    State s[states];
+    for(int i = 0;i < states; i++)
+    {
+        s[i].set(symbs, finalStates);
+    }
+    Groups = 2; //Primele doua partitii
     MINIMIZATION(s);
     PrintPartitions(s);
-    run(s);
-
     return 0;
 }
