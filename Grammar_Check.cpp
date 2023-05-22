f4b7f21053dfb0275c7676433789a8cb5042d5bd
Barzoius <95474661+Barzoius@users.noreply.github.com>
2023-05-22 04:39:22 -1100

    Update Grammar_Check.cpp

--- Grammar_Check.cpp
+++ Grammar_Check.cpp
@@ -1,21 +1,38 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <algorithm>
 
-bool valideaza(const std::unordered_map<char, std::vector<std::pair<char, std::string>>>& gramatica, char start_symbol, const std::string& ward) {
-    if (ward.empty() && gramatica.at(start_symbol).size() > 0 && gramatica.at(start_symbol)[0].second == "l") {
-        std::cout << "Cuvant Valid" << std::endl;
-        return true;
+bool validate(const std::unordered_map<char, std::vector<std::pair<char, std::string>>>& grammar, char start_symbol, const std::string& word)
+{
+    std::cout << "Validating word: " << word << std::endl;
+
+    if (word.empty())
+    {
+        const auto& start_rules = grammar.at(start_symbol);
+        if (std::find_if(start_rules.begin(), start_rules.end(), [](const std::pair<char, std::string>& rule)
+        { return rule.first == 'l' && rule.second.empty(); }) != start_rules.end())
+        {
+            std::cout << "Valid word" << std::endl;
+            return true;
+        }
     }
 
-    for (const auto& regula : gramatica.at(start_symbol)) {
-        if (islower(regula.first) && !ward.empty() && ward[0] == regula.first) {
-            if (valideaza(gramatica, regula.second[0], ward.substr(1))) {
+    for (const auto& rule : grammar.at(start_symbol))
+    {
+        std::cout << "Checking rule: " << rule.first << " -> " << rule.second << std::endl;
+
+        if (std::islower(rule.first) && !word.empty() && word[0] == rule.first)
+        {
+            if (validate(grammar, rule.second[0], word.substr(1)))
+            {
                 return true;
             }
         }
-        else if (isupper(regula.first)) {
-            if (valideaza(gramatica, regula.first, regula.second + ward)) {
+        else if (std::isupper(rule.first))
+        {
+            if (validate(grammar, rule.first, rule.second + word))
+            {
                 return true;
             }
         }
@@ -24,22 +41,26 @@ bool valideaza(const std::unordered_map<char, std::vector<std::pair<char, std::s
     return false;
 }
 
-int main() {
-    std::unordered_map<char, std::vector<std::pair<char, std::string>>> gramatica = {
+int main()
+{
+    std::unordered_map<char, std::vector<std::pair<char, std::string>>> grammar = {
             {'S', {{'a', "A"}, {'d', "E"}}},
             {'A', {{'a', "B"}, {'a', "S"}}},
             {'B', {{'b', "C"}}},
             {'C', {{'b', "D"}, {'b', "B"}}},
-            {'D', {{'c', "D"}, {'l', ""}}},
-            {'E', {{'l', ""}}}
-    };
+            {'D', {{'c', "D"}, {'e', ""}, {'l', " "}}},
+            {'E', {{'d', ""}}}
+};
 
-    char start_symbol = 'S';
-    std::string ward = "aabbdcc";
 
-    if (!valideaza(gramatica, start_symbol, ward)) {
-        std::cout << "Cuvantul " << ward << " este invalid" << std::endl;
-    }
+char start_symbol = 'S';
+std::string word = "aabbcce";
 
-    return 0;
+if (!validate(grammar, start_symbol, word))
+{
+std::cout << "The word '" << word << "' is invalid." << std::endl;
 }
+
+return 0;
+}
+
